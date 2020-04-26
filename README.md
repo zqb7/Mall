b3f48993872db398692f1371ec1f1e0f1a654999
bzyy <sadeye@qq.com>
2020-04-26 11:40:39 +0800

    完成首页分类样式

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mall
+> 学习uni-app
+
+
+
+### 参考
+- [nideshop小程序](https://github.com/tumobi/nideshop-mini-program)
+- uni-app电商模板
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,24 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="search">
+			<input class="search-input" type="text" placeholder="输入关键字搜索" />
+		</view>
+		
+		<!-- 轮播图 -->
+		<view>
+			<swiper  class="banner"  :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item v-for="item in carouseList">
+					<image :src="item.src" />
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		<!-- 分类 -->
+		<view class="channel">
+			<view class="channel-item" v-for="item in channelList">
+				<image :src="item.img_src" />
+				<text>{{item.name}}</text>
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,7 +27,33 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
+				carouseList: [
+					{
+						"src": "http://yanxuan.nosdn.127.net/65091eebc48899298171c2eb6696fe27.jpg"
+					},
+					{
+						"src": "http://yanxuan.nosdn.127.net/bff2e49136fcef1fd829f5036e07f116.jpg"
+					},{
+						"src": "http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg"
+					}
+				],
+				channelList: [
+					{
+						"name": "居家",
+						"img_src": "/static/images/channel/c1.png"
+					},{
+						"name": "餐厨",
+						"img_src": "/static/images/channel/c2.png"
+					},{
+						"name": "配件",
+						"img_src": "/static/images/channel/c3.png"
+					}
+				]
 			}
 		},
 		onLoad() {
@@ -23,30 +65,39 @@
 	}
 </script>
 
-<style>
-	.content {
+<style lang="scss">
+	.search {
+		top:30upx;
+		width: 100%;
+		margin: 20upx 0;
+		.search-input {
+			background: rgba(255,255,255,.6);
+			text-align: center;
+			font-size: 30upx;
+			border-radius: 20upx;
+		}
+	};
+	.banner {
+		height: 417rpx;
+		uni-image {
+			width: 100%;
+		}
+	}
+	.channel {
 		display: flex;
-		flex-direction: column;
 		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		.channel-item {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+		uni-image {
+			width: 58upx;
+			height: 58upx;
+			border-radius: 50%;
+			margin-bottom: 10upx;
+		}
 	}
 </style>
